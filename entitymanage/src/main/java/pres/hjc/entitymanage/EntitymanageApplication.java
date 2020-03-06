837043b34680e1bf4a189900191c2cf2bf322540
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-03-06 12:24:39 +0800

    update

---
+++ entitymanage/src/main/java/pres/hjc/entitymanage/EntitymanageApplication.java
@@ -0,0 +1,19 @@
+package pres.hjc.entitymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: HJC
+ * Date: 2020/3/6
+ * Time: 12:20
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class EntitymanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitymanageApplication.class, args);
+    }
+}
